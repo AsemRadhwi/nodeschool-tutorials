e805fd0e95c89fd050ddce6bc0550d58b7d0dcf5
Asem Radhwi <asem.radhwi@gmail.com>
2014-10-09 21:41:53 +0300

    after completing fourth challenge

--- 04_program.js
+++ 04_program.js
@@ -2,7 +2,9 @@ var fs = require('fs');
 
 var filename = process.argv[2];
 
-var buf = fs.readFileSync(filename);
+var buf = fs.readFile(filename, 'utf-8', function(err, data){
+    console.log((data.toString().split("\n").length-1));
+});
 
-console.log((buf.toString().split("\n").length-1));
+
 
